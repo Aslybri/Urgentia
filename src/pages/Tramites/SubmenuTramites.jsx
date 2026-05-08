9c3ce73edbb7d3e15aea246bd55062278ca5a131
dashanino <nino.dasha@uces.edu.co>
2026-05-08 14:28:27 -0500

    recuperación de cositas borradas

--- src/pages/Tramites/SubmenuTramites.jsx
+++ src/pages/Tramites/SubmenuTramites.jsx
@@ -9,9 +9,27 @@ function SubmenuTramites() {
         <h2 className="title title-small">¿Qué trámite necesitas?</h2>
 
         <div className="submenu-grid">
+          <button className="submenu-btn">
+            Autorización de examen / procedimiento / cirugía
+          </button>
+
+          <button className="submenu-btn">
+            Autorización para valoración con especialista
+          </button>
+
+          <button className="submenu-btn">
+            Asignación de cita por consulta externa
+          </button>
+
           <button className="submenu-btn">Renovar fórmula</button>
 
-          <button className="submenu-btn">Asignar cita</button>
+          <button className="submenu-btn">
+            Vencimiento de orden para examen / procedimiento / cirugía
+          </button>
+
+          <button className="submenu-btn">
+            Vencimiento de orden para valoración con especialista
+          </button>
         </div>
 
         <button className="back" onClick={() => navigate("/tramites")}>
--- src/pages/Tramites/Tramites.jsx
+++ src/pages/Tramites/Tramites.jsx
@@ -37,3 +37,17 @@ function Tramites() {
 }
 
 export default Tramites;
+//<select
+//                 value={epsSeleccionada}
+//                 onChange={(e) => setEpsSeleccionada(e.target.value)}
+//               >
+//                 <option value="">Seleccione su EPS</option>
+//                 <option value="Nueva EPS">Nueva EPS</option>
+//                 <option value="Salud Total EPS">Salud Total EPS</option>
+//                 <option value="Compensar EPS">Compensar EPS</option>
+//                 <option value="EPS Sura">EPS Sura</option>
+//                 <option value="Famisanar EPS">Famisanar EPS</option>
+//                 <option value="Mutual Ser EPS">Mutual Ser EPS</option>
+//                 <option value="Coosalud EPS">Coosalud EPS</option>
+//                 <option value="Capital Salud">Capital Salud</option>
+//               </select>
